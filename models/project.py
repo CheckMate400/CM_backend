466cbcb6555c0dc072c9d3c7239c3cfe19e4fee2
CheckMate400 <checkmateisrael1@gmail.com>
2025-06-15 12:55:42 +0300

    Update project.py

--- models/project.py
+++ models/project.py
@@ -1,8 +1,9 @@
-# models/project.py
-
 from pydantic import BaseModel
 
-class ProjectCreate(BaseModel):
+class ProjectCreateRequest(BaseModel):
     project_name: str
     subject: str
     num_questions: int
+    num_tests: int
+    project_type: str
+    expected_average: int | None = None
